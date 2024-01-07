b77d309a5b15af62b18f7f21a96ae7d93b163ce4
Srikar <t1srikar@gmail.com>
2024-01-08 01:15:05 +0530

    Style updates

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const searchBar = document.querySelector(".search-bar");
+const searchQuery = document.getElementById('query-text');
+
+searchBar.addEventListener("click", searchBarClick);
+searchQuery.addEventListener("focus", showDropdown);
+searchQuery.addEventListener("focusout", hideDropdown);
+
 function search() {
-    const searchQuery = document.getElementById('query-text');
     console.log(searchQuery.value);
     window.location.href = "https://www.google.com/search?q=" + searchQuery.value;
+}
+
+function searchBarClick() {
+    searchQuery.focus();
+}
+
+var dropdown = document.querySelector('.dropdown');
+function showDropdown() {
+    dropdown.style.display = 'block';
+}
+
+function hideDropdown() {
+    dropdown.style.display = 'none';
 }
